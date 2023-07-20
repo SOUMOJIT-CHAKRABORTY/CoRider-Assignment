f1bd50f1647213aab8aa0e36feebba47cabca764
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-20 21:22:09 +0530

    issue resolve

--- chat-screen/src/App.tsx
+++ chat-screen/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import GetChats from "./api/Chats";
+import GetChats from "./api/chats";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
